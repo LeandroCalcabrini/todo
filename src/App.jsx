cec096844688696f1dc900b75e0c1faebaf80e5b
Leandro <leacalca@gmail.com>
2025-06-23 12:43:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+
+  return (
+    <>
+    <form className="todoForm">
+      <input
+      className="todoInput"
+      type="text"/>
+      <button type="submit">Add</button>
+    </form>
+     
+    </>
+  )
+}
+
+export default App
